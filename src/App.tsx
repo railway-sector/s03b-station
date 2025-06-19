626183f5778f101995755904a567803bc23948b6
EijiGorilla <emasuz@hotmail.com>
2025-06-19 10:16:14 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+import "@arcgis/map-components/components/arcgis-legend";
+import "@esri/calcite-components/dist/components/calcite-shell";
+import "@esri/calcite-components/dist/calcite/calcite.css";
+import { CalciteShell } from "@esri/calcite-components-react";
+import MapDisplay from "./components/MapDisplay";
+import ActionPanel from "./components/ActionPanel";
+import Header from "./components/Header";
+import MainChart from "./components/MainChart";
+import UndergroundSwitch from "./components/UndergroundSwitch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CalciteShell>
+        <ActionPanel />
+        <UndergroundSwitch />
+        <MapDisplay />
+        <MainChart />
+        <Header />
+      </CalciteShell>
     </div>
   );
 }
